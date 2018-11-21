8b699e0aaa5a1a1f2e5d75fca87b36441b9184cc
Miguel <migueljuniorandrade@gmail.com>
2018-11-21 15:54:23 +0000

    Add project files.

---
+++ PostInstall/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Application Loading
+
+        'Manually add the executables to the program (code below), read them from a file or search in a directory
+        Dim p1 As New ProgramExe("teste1", "-s", "Programa Teste1")
+        Dim p2 As New ProgramExe("teste2", "-s", "Programa Teste2")
+        CheckedListBox1.Items.Add(p1)
+        CheckedListBox1.Items.Add(p2)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Run executables
+        For Each i In CheckedListBox1.CheckedItems
+            Dim item As ProgramExe = CType(i, ProgramExe)
+            'MsgBox(item.exe & vbNewLine & item.args)
+            'run prog
+            'Dim psi As New ProcessStartInfo(item.exe, item.args)
+            Dim psi As ProcessStartInfo = item.toProcessStartInfo()
+            Dim p As New Process() With {
+                .StartInfo = psi
+            }
+            p.Start()
+            p.WaitForExit()
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Handles 'Selecionar tudo' button click
+    ''' Marks as checked every item in the checkedlistbox
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Check everything
+        For i As Integer = 0 To CheckedListBox1.Items.Count - 1
+            CheckedListBox1.SetItemChecked(i, True)
+        Next
+    End Sub
+End Class
---
+++ PostInstall/ProgramExe.vb
@@ -0,0 +1,30 @@
+﻿Public Class ProgramExe
+
+    Public ReadOnly Property exe As String
+    Public ReadOnly Property args As String
+    Public ReadOnly Property DisplayName As String
+
+    ''' <summary>
+    ''' Creates a new program executable object containing a exe path, possible arguments to be used at launch and a display name.
+    ''' </summary>
+    ''' <param name="exe">EXE Path</param>
+    ''' <param name="args">EXE Arguments</param>
+    ''' <param name="DisplayName">Name to shown in the CheckedListBox. Defaults to the exe name if none is provided.</param>
+    Public Sub New(ByVal exe As String, ByVal args As String, Optional ByVal DisplayName As String = "")
+        Me.exe = exe
+        Me.args = args
+        Me.DisplayName = IIf(DisplayName = "", IO.Path.GetFileName(exe), DisplayName)
+    End Sub
+
+    ''' <summary>
+    ''' Returns a new ProcessStartInto object for the EXE
+    ''' </summary>
+    ''' <returns>new ProcessStartInfo with exe and args</returns>
+    Public Function toProcessStartInfo() As ProcessStartInfo
+        Return New ProcessStartInfo(exe, args)
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return DisplayName
+    End Function
+End Class
